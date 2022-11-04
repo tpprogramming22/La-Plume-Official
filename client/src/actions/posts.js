45f0fee7cbacd30f22ffad676fda30314a8d4981
ted <tpprogrammingcontact@gmail.com>
2022-11-04 18:57:54 +0100

    created delete functionality

--- client/src/actions/posts.js
+++ client/src/actions/posts.js
@@ -27,3 +27,15 @@ export const updatePost = (id, post) => async (dispatch) => {
     console.log(error.message);
   }
 }
+
+export const deletePost = (id) => async (dispatch) => {
+  try {
+    await api.deletePost(id);
+
+    dispatch({ type: 'DELETE', payload: id })
+
+  }
+  catch (error) {
+    console.log(error)
+  }
+}
--- client/src/components/Posts/Post/Post.js
+++ client/src/components/Posts/Post/Post.js
@@ -11,8 +11,13 @@ import MoreHorizIcon from '@material-ui/icons/MoreHoriz'
 
 import moment from 'moment'
 
+import { useDispatch } from "react-redux";
+
+import { deletePost } from "../../../actions/posts"; 
+
 const Post = ({ post, setCurrentId }) => {
     const classes = useStyles();
+    const dispatch = useDispatch();
     return (
         <Card className={classes.card}>
             <CardMedia className={classes.media} image={post.selectedFile} title={post.title}/>
@@ -38,7 +43,7 @@ const Post = ({ post, setCurrentId }) => {
                     Like
                     {post.likeCount}
                 </Button>
-                <Button size="small" color="primary" onClick={()=>{}}>
+                <Button size="small" color="primary" onClick={()=> dispatch(deletePost(post._id))}>
                     <DeleteIcon fontSize="small"/>
                     Delete
                 </Button>
